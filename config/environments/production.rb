174cf30d2a60481de4200dc3aefc1d81fb51befb
kat-gie <gielenkatie@gmail.com>
2017-01-07 21:06:57 -0600

    fixed error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,6 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { 'https://flixter-katgie.herokuapp.com/' }
+  #config.action_mailer.default_url_options = { 'https://flixter-katgie.herokuapp.com/' }
+  config.action_mailer.default_options = { from: "https://flixter-katgie.herokuapp.com/" }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
